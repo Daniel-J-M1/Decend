7c640f03a1fcbe63396c8836ceea503defdcaa9c
Daniel John McLaughlan <daniel-j-m@live.com>
2021-11-17 03:51:04 +0000

    Changed Movement and CardLogic script, Project Platform, and Camera View
    Developing the cards further, after realising the previous movement would not suit the purpose I wanted for it. Hoping the new system will be more successful.

--- Assets/CardLogic.cs
+++ Assets/CardLogic.cs
@@ -7,6 +7,43 @@ public class CardLogic : MonoBehaviour
 {
     public TMP_Text tmp;
     public bool IsMouseOver = false;
+    public GameObject Card;
+    private Vector3 Offset;
+
+    private void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D Target = Physics2D.OverlapPoint(MousePosition);
+
+            if (Target)
+            {
+                Card = Target.transform.gameObject;
+                Offset = Card.transform.position - MousePosition;
+            }
+        }
+
+        if (Card)
+        {
+            Card.transform.position = MousePosition + Offset;
+        }
+
+        if (Input.GetMouseButtonUp(0) && Card)
+        {
+            Card = null;
+        }
+
+        print(MousePosition);
+    }
+
+
     private void OnMouseOver()
     {
         IsMouseOver = true;
--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -4,52 +4,59 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    public GameObject[] Cards;
     public GameObject Card;
     public CardLogic CL;
     float FMovingSpeed = 1f;
+    public float BreadCrumbs = 0f;
+    private Transform CardSwipe;
 
     SpriteRenderer SR;
 
     void Start()
     {
+        Cards = GameObject.FindGameObjectsWithTag("Cards");
         SR = Card.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && CL.IsMouseOver)
-        {
-            Vector2 Pos = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            Card.transform.position = Pos;
-        }
-        else
-        {
-            Card.transform.position = Vector2.MoveTowards(Card.transform.position, new Vector2(0, 0), FMovingSpeed);
-        }
-        //Right Swipe
-        if (Card.transform.position.x > 2)
-        {
-            SR.color = Color.green;
-            if (Input.GetMouseButton(1))
+
+            if (Input.GetMouseButton(0) && CL.IsMouseOver)
             {
-                CL.InduceRight();
+                Vector2 Pos = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                Card.transform.position = Pos;
             }
-        }
-        //Left Swipe
-        else if(Card.transform.position.x < -2)
-        {
-            SR.color = Color.red;
-            if (Input.GetMouseButton(1))
+            else
             {
-                CL.InduceLeft();
+                Card.transform.position = Vector2.MoveTowards(Card.transform.position, new Vector2(0, 0), FMovingSpeed);
             }
-        }
-        //Neutral
-        else
-        {
-            SR.color = Color.white;
-            CL.InduceNeutral();
-        }
+            //Right Swipe
+            if (Card.transform.position.x > 2)
+            {
+                SR.color = Color.green;
+                if (Input.GetMouseButtonUp(0))
+                {
+                    CL.InduceRight();
+                    print("Press Right");
+                }
+            }
+            //Left Swipe
+            else if (Card.transform.position.x < -2)
+            {
+                SR.color = Color.red;
+                if (Input.GetMouseButtonUp(0))
+                {
+                    CL.InduceLeft();
+                    print("Press Left");
+                }
+            }
+            //Neutral
+            //else
+            //{
+            //    SR.color = Color.white;
+            //    CL.InduceNeutral();
+            //}
     }
 }
